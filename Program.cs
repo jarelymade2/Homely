2702fbdbffd43e18eece29ac8bffd712099e5374
davidllamocca <carlos_llamocca@usmp.pe>
2025-08-28 22:11:03 -0500

    data3

--- Program.cs
+++ Program.cs
@@ -1,29 +1,43 @@
+using Microsoft.EntityFrameworkCore;
+using StayGo.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// MVC
 builder.Services.AddControllersWithViews();
+
+// ✅ DbContext con SQLite
+builder.Services.AddDbContext<StayGoContext>(options =>
+{
+    var cs = builder.Configuration.GetConnectionString("Sqlite");
+    options.UseSqlite(cs);
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+// (Opcional) Crear la BD si no existe
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<StayGoContext>();
+    db.Database.EnsureCreated(); // usa migraciones si prefieres: db.Database.Migrate();
+}
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
