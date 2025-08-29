45053d8f832511c11d8f78e3296aac37ce8c0741
Jazarelly Zapata <jazarelly_zapata@usmp.pe>
2025-08-28 21:55:46 -0500

    home

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,19 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('.counter-control').forEach(function (control) {
+        const input = control.querySelector('input');
+        const minusBtn = control.querySelector('.btn-counter-minus');
+        const plusBtn = control.querySelector('.btn-counter-plus');
 
-// Write your JavaScript code.
+        minusBtn.addEventListener('click', function () {
+            let value = parseInt(input.value);
+            if (value > 0) {
+                input.value = value - 1;
+            }
+        });
+
+        plusBtn.addEventListener('click', function () {
+            let value = parseInt(input.value);
+            input.value = value + 1;
+        });
+    });
+});
