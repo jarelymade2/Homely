4b0f10bdfff187b1a5d2abf514c057f94a0c747c
Jarely M. <jarely643@gmail.com>
2025-08-28 22:32:18 -0500

    datos simulados

--- Controllers/PropiedadController.cs
+++ Controllers/PropiedadController.cs
@@ -1,16 +1,28 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using StayGo.Data;
+using StayGo.Models;
 
 namespace StayGo.Controllers;
-public class PropiedadesController : Controller
+public class PropiedadController : Controller
 {
     private readonly StayGoContext _db;
-    public PropiedadesController(StayGoContext db) => _db = db;
+    public PropiedadController(StayGoContext db) => _db = db;
 
     public async Task<IActionResult> Index()
     {
-        var props = await _db.Propiedades.AsNoTracking().ToListAsync();
-        return View(props);
+         var modelo = await _db.Propiedades.ToListAsync();
+
+            if (!modelo.Any())
+            {
+                modelo = new List<Propiedad>
+                {
+                    new Propiedad { Nombre = "Casa de Playa", Ubicación = "Lima", PrecioPorNoche = 200 },
+                    new Propiedad { Nombre = "Departamento céntrico", Ubicación = "Cusco", PrecioPorNoche = 150 },
+                    new Propiedad { Nombre = "Cabaña en la montaña", Ubicación = "Arequipa", PrecioPorNoche = 120 }
+                };
+            }
+
+            return View(modelo);
     }
 }
